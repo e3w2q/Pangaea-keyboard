e65caccea44e6d2f18410435e29e08a5d5d4f888
otahinosame <otahinosame@gmail.com>
2022-12-24 23:11:12 +0900

    enable RGB in via keymap

--- firmware/1.2/keymaps/otahinosame/rules.mk
+++ firmware/1.2/keymaps/otahinosame/rules.mk
@@ -3,3 +3,4 @@ OLED_ENABLE = no
 OLED_DRIVER = SSD1306
 VIA_ENABLE = yes
 LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
--- firmware/1.2/keymaps/via/rules.mk
+++ firmware/1.2/keymaps/via/rules.mk
@@ -2,3 +2,5 @@ ENCODER_ENABLE = yes
 OLED_ENABLE = no
 OLED_DRIVER = SSD1306
 VIA_ENABLE = yes
+LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
