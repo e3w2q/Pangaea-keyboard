3e118e1ef649b3cac089aedcb2742ca16b19df91
otahinosame <otahinosame@gmail.com>
2023-12-20 23:48:37 +0900

    Apply ENCODER_MAP

--- firmware/pangaea/keymaps/otahinosame/keymap.c
+++ firmware/pangaea/keymaps/otahinosame/keymap.c
@@ -151,3 +151,13 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
   return false;
 }
 #endif
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN), ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI),           ENCODER_CCW_CW(RGB_SAD, RGB_SAI)  },
+    [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI),           ENCODER_CCW_CW(RGB_SPD, RGB_SPI)  },
+    [3] =   { ENCODER_CCW_CW(KC_UP, KC_DOWN), ENCODER_CCW_CW(KC_LEFT, KC_RGHT)  },
+    //                Left Encoder                                Right Encoder
+};
+#endif
